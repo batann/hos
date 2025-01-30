fcbc1c2ea4119c36353db4defc709c9de2106603
batan <122584289+batann@users.noreply.github.com>
2025-01-30 22:16:34 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ if [[ -f /etc/hosts ]];then
 sudo mv /etc/hosts /etc/hosts.original
 fi
 sudo chown root:root $DIR_BASE/*
-sudo mv hosts.1 $DIR_TARGET
-sudo mv hosts.2 $DIR_TARGET
+sudo mv hosts.1 $DIR_TARGET/hosts
+sudo mv hosts.2 $DIR_TARGET/hosts.2
 sudo mv lc-hosts /usr/bin
 sudo chmod a+x /usr/bin/lc-*
 sudo trash $DIR_BASE
